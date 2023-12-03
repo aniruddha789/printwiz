ac79930cbf995c1c45d1ec3c6082429855fdb7cb
aniruddha789 <aniruddhaborkar45@gmail.com>
2023-12-04 02:09:56 +0530

    Added contact us button

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -15,6 +15,8 @@ function NavBar() {
     return "Hello";
   };
 
+  const contactLink = "https://api.whatsapp.com/send/?phone=%2B917721906862&text&type=phone_number&app_absent=0";
+
   return (
     <>
       <div className="nav">
@@ -23,7 +25,11 @@ function NavBar() {
           <Link to="#home" className="brandContainer">
             <img src={logo} className="brandLogo"></img>
           </Link>
-          <FaBars />
+          {/* <FaBars /> */}
+        
+          <Button href={contactLink} className="getStartedButton" >
+                  Contact Us
+            </Button>
           
           {/* <div className="dummy1"></div>
           <div className="dummy2"></div>
